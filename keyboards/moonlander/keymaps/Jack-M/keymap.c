a41031b40b4c01d68c23d44b106e30dc9b7fe6f7
Jan-Frederick Musiol <jan@musiol-bs.de>
2026-09-19 17:28:07 +0200

    Fixed double page up on moonlander

--- keyboards/moonlander/keymaps/Jack-M/keymap.c
+++ keyboards/moonlander/keymaps/Jack-M/keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENTER),
-        KC_LCTL, KC_LGUI, KC_LOCK, KC_HYPR, KC_LALT,          _______, TG(1),            KC_RALT, KC_MEH,  KC_APP, KC_RGUI,  KC_RCTRL,
+        KC_LCTL, KC_LGUI, KC_LOCK, KC_HYPR, KC_LALT,          _______, TG(1),            KC_RALT, KC_MEH,  KC_APP,  KC_RGUI, KC_RCTRL,
                                             KC_SPC,  TT(2),   TT(3),   TT(2),   TT(3),   KC_BSPC
     ),
     [_GAME] = LAYOUT_moonlander(
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_LOWER] = LAYOUT_moonlander(
         KC_GRV,  _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, _______, _______, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_BSPC,
-        KC_NUBS, KC_HOME, KC_PGUP, KC_UP,   KC_PGUP, KC_INS,  _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_RBRC,
+        KC_NUBS, KC_HOME, KC_PGUP, KC_UP,   KC_PGDN, KC_INS,  _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_RBRC,
         KC_CAPS, KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_CALC, KC_BSLS,
         _______, _______, _______, _______, _______, _______,                   _______, KC_P1,   KC_P2,   KC_P3,   KC_PENT, KC_ENT,
         _______, _______, _______, _______, _______,          _______, _______,          KC_P0,   KC_P000, KC_PDOT, KC_PCMM, _______,
